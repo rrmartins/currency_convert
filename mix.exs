ae6f43343633369385e3a046e1ab14fcb2e7a240
Rodrigo Martins <rrmartinsjg@gmail.com>
2024-01-02 08:46:51 +0000

    add new configs

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule CurrencyConvert.MixProject do
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -23,7 +25,24 @@ defmodule CurrencyConvert.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:hex, "~> 0.20"}
+      {:ex_doc, "~> 0.18", only: :dev},
+      {:httpoison, "~> 1.7"},
+      {:jason, "~> 1.2"}
+    ]
+  end
+
+  defp description do
+    """
+    An Elixir library for converting currencies.
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib"],
+      maintainers: ["Rodrigo Martins"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/rrmartins/currency_convert"}
     ]
   end
 end
