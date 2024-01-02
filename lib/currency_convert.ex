eb5290d8cb158e7f0911154acb75767f0ec345f3
Rodrigo Martins <rrmartinsjg@gmail.com>
2024-01-02 08:47:44 +0000

    feat: calc convert number

--- lib/currency_convert.ex
+++ lib/currency_convert.ex
@@ -3,16 +3,44 @@ defmodule CurrencyConvert do
   Documentation for `CurrencyConvert`.
   """
 
-  @doc """
-  Hello world.
+  # API URL
+  @rates_url "https://api.exchangerate-api.com/v4/latest/"
 
-  ## Examples
+  def convert(amount, from_currency, to_currency) do
+    get_rates(from_currency, to_currency)
+    |> handle_rates()
+    |> convert_amount(amount, to_currency, from_currency)
+  end
 
-      iex> CurrencyConvert.hello()
-      :world
+  defp get_rates(from_currency, to_currency) do
+    # Get the rates from the API
+    case HTTPoison.get("#{@rates_url}#{from_currency}") do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        {:ok, Jason.decode!(body)["rates"]}
 
-  """
-  def hello do
-    :world
+      {:ok, %HTTPoison.Response{status_code: status_code, body: body}} ->
+        {:error, "Request failed with status code #{status_code} and message #{body}"}
+
+      {:error, error} ->
+        # Return the error
+        error
+    end
+  end
+
+  defp handle_rates(:ok, rates) do
+    # Return the rates
+    rates
+  end
+
+  defp handle_rates(error), do: error
+
+  defp convert_amount({:error, error}, _rates, _to_currency, _from_currency) do
+    # Return the error
+    raise "Error: #{error}"
+  end
+
+  defp convert_amount(rates, amount, to_currency, from_currency) do
+    # Convert the amount
+    amount * Map.get(rates, to_currency) / Map.get(rates, from_currency)
   end
 end
